f6486918f3367cce0ebf5bc8e37254a6f404e47e
newchar <crazy-yj@qq.com>
2019-06-12 13:47:48 +0800

    更新webView组件

---
+++ app/src/main/java/com/newchar/devnews/NewCharWebViewClient.java
@@ -0,0 +1,15 @@
+package com.newchar.devnews;
+
+import android.webkit.WebViewClient;
+
+/**
+ * @author wenliqiang wenliqiang@100tal.com
+ * date            2019-06-12
+ * @since 当前版本描述，
+ * @since 迭代版本描述
+ */
+public class NewCharWebViewClient extends WebViewClient {
+
+
+
+}
--- app/src/main/java/com/newchar/devnews/WebViewActivity.java
+++ app/src/main/java/com/newchar/devnews/WebViewActivity.java
@@ -1,6 +1,10 @@
 package com.newchar.devnews;
 
 import android.os.Bundle;
+import android.view.ViewParent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +17,59 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class WebViewActivity extends AppCompatActivity {
 
-    public static final String WEB_URL = "url";
+    private WebView mWebView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final String url = getIntent().getStringExtra(WEB_URL);
+        setContentView(R.layout.activity_web_view);
+        inflateWebView();
+    }
 
+    private WebView createWebView() {
+        WebView webView = new WebView(getApplicationContext());
+        webView.setWebViewClient(createWebViewClient());
+        return webView;
+    }
+
+    private WebViewClient createWebViewClient() {
+        return new NewCharWebViewClient();
+    }
+
+
+    private void inflateWebView() {
+        FrameLayout flWebViewContainer = findViewById(R.id.flWebViewContainer);
+        if (flWebViewContainer.getChildCount() > 0) {
+            mWebView = (WebView) flWebViewContainer.getChildAt(0);
+        } else {
+            mWebView = createWebView();
+            flWebViewContainer.addView(mWebView);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mWebView != null) {
+            mWebView.onPause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        destroyWebView();
+        super.onDestroy();
+    }
+
+    private void destroyWebView() {
+        if (mWebView != null) {
+            ViewParent parentView = mWebView.getParent();
+            if (parentView instanceof FrameLayout) {
+                ((FrameLayout) parentView).removeAllViews();
+            }
+            mWebView.destroy();
+            mWebView = null;
+        }
     }
 
 
